8e0caca70bd2c3b40e4b41aad14aa51faf605fb1
turutete <caromper@gmail.com>
2024-12-26 21:54:15 +0100

    Script de modelado dinámico de arco electrico

--- Scripts/Arco_Dinamico.m
+++ Scripts/Arco_Dinamico.m
@@ -12,27 +12,83 @@ alfa=49.0874;
 beta=1.4614;
 
 dI=0.01;
-It=(-512:511).*dI;
+It=(-2048:2047).*dI;
 Vt=(alfa*Rc.*It)./(atan(beta.*It).*It.*Rc+alfa);
 
 % Modelo de circuito de descarga
 Rl=10;
-Vg=75;
 
-Vt2=Vg-It.*Rl;
-
-figure(1);plot(It,Vt,It,Vt2);
-
-% Cálculo rápido puntos corte
-F=Vt2-Vt;
-L=length(F);
-F2=[F(1)^2 F(1:L-1).*F(2:L)];
-
-index_cross=find(F2<0);
+% Ejemplo de Arco DC
+Fs=12500;
+%Vg_vector=[75*ones(1,512) 150*ones(1,512)];
+n=1:1024;
+Vg_vector(n)=150*sin(2*pi*50*n/Fs);
 
 
-Iarc=It(index_cross);
-Varc=(alfa*Rc.*Iarc)./(atan(beta.*Iarc).*Iarc.*Rc+alfa);
+
+Vt_vector=[];
+It_vector=[];
+
+for index=1:length(Vg_vector)
+  
+  % Cálculo de puntos de trabajo
+  Vt2=Vg_vector(index)-It.*Rl;
+
+  % Cálculo rápido puntos corte
+  index_corte=Puntos_Corte(Vt,Vt2);
+  if length(index_corte)==0
+    error("El modelo V-I estático tiene pocos puntos");
+  endif
+  
+  It_corte=(index_corte-2049).*dI;
+  Vt_corte=Vg_vector(index)-It_corte.*Rl;
+  
+  if index==1
+    [Itn,indimax]=max(It_corte);
+    Vtn=Vt_corte(indimax);
+    Vtprev=Vtn;
+    Itprev=Itn;
+    Ptprev=Vtn*Itn;
+  else
+    Pn_vect=It_corte.*Vt_corte;
+    Pnprev_vect=Ptprev*ones(size(index_corte));
+    Pdif=abs(Pnprev_vect-Pn_vect);
+    [Pdifmin,indmin]=min(Pdif);
+    Vtn=Vt_corte(indmin);
+    Itn=It_corte(indmin);
+    Vtprev=Vtn;
+    Itprev=Itn;
+    Ptprev=Vtn*Itn;
+  endif
+  
+  Vt_vector(index)=Vtn;
+  It_vector(index)=Itn;
+  
+endfor
+
+% Modelo dinámico del arco eléctrico
+tau=100e-6;
+
+A=1+2*tau*Fs;
+B=1-2*tau*Fs;
+
+Nom=[1 1];
+Den=[A B];
+
+Iarc=filter(Nom,Den,It_vector);
+Varc=filter(Nom,Den,Vt_vector);
+
+figure(1);plot(Varc);
+figure(2);plot(Iarc);
+
+
+
+  
+
+
+
+
+
 
 
 
